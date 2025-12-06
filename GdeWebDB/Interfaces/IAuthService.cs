b7769448f794dffb4efab06e61f5b70cc9ccc0a6
David Peter Horvath <hhorvath.david.peter@gmail.com>
2025-12-06 15:19:58 +0100

    Initial commit

---
+++ GdeWebDB/Interfaces/IAuthService.cs
@@ -0,0 +1,27 @@
+﻿using GdeWebModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GdeWebDB.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<LoginResultModel> LoginWithGoogle(string email, string fullName, string picture);
+        Task<LoginResultModel> Login(LoginModel credential);
+
+        Task<LoginResultModel> Auth(LoginModel credentials);
+
+        Task<LoginResultModel> Forgot(ForgotModel model);
+
+        Task<ResultModel> GetUserTokenExpirationDate(int userId, DateTime expirationDate);
+
+        Task<ResultModel> AddUserTokenExpirationDate(int userId, string token, DateTime expirationDate);
+
+        Task<LoginUserModel> GetUser(int userId);
+
+        Task<ResultModel> UserValidation(int userId, string userGuid);
+    }
+}
